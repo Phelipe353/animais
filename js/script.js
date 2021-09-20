a6a12a5d566bc5d136dc23e2baa0ab31f7bec079
Phelipe <88689679+Phelipe353@users.noreply.github.com>
2021-09-20 17:14:13 -0300

    versao final

---
+++ js/script.js
@@ -0,0 +1,22 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropDownMenu.js';
+import initenuMobile from './modules/menuMobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
+import initFuncionamento from './modules/funcionamento.js';
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropDownMenu();
+initenuMobile();
+initAnimaNumeros();
+initFuncionamento();
